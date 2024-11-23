689475bed8de4f2a804b9becb00385de0190d3a7
nikita nimore <992nikitanimore@gmail.com>
2024-11-23 18:34:23 +0530

    complete that program

---
+++ C_CheckParenthes.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class CheckParenthes {
+    // Function to check if brackets are balanced or not.
+    static boolean isParenthesisBalanced(String s) {
+        int n = s.length();
+        if(n%2!=0){
+            return false;
+        }
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i =0; i<n; i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch =='[' || ch == '{'){
+            stack.push(ch);
+            }
+            else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.peek();
+                if(ch==')' && top!='('){
+                    return false;
+                }else if(ch==']' && top!='['){
+                    return false;
+                }else  if(ch=='}' && top!='{'){
+                    return false;
+                }
+                
+                else {
+                    stack.pop();
+                    
+            }
+        }
+        
+        }
+        return (stack.size()==0);
+        
+    }
+
+    public static void main (String[] args) {
+		
+		String s="()[{}()]";
+		if(isParenthesisBalanced(s)==true)
+		System.out.println("True");
+		else
+		System.out.println("False");
+	}
+
+}
+
+
