8f1b41e73ce75ab62bdbc61615936c3978cf36f1
nikita nimore <992nikitanimore@gmail.com>
2024-11-26 13:59:11 +0530

    compelete program Reverse word String

--- G_ReverseWordString.java
+++ G_ReverseWordString.java
@@ -1,6 +1,26 @@
 
-
+import java.util.*;
 
 class G_ReverseWordString{
-    
+    static String reverseWords(String str) {
+        List<String> words = new ArrayList<>();
+        String[] parts = str.split("\\.");
+        
+        for (String word : parts) {
+            if (!word.isEmpty()) {
+                words.add(word);
+            }
+        }
+        
+        // Reverse the words
+        Collections.reverse(words);
+        
+        // Join the reversed words back into a string
+        return String.join(".", words);
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello.Nikita...I.am.fine.yrr";
+        System.out.println(reverseWords(str));
+    }
 }
