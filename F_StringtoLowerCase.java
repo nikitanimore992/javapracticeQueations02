1c0ea238f7d41dc32fa59face0d7c599aedadf86
nikita nimore <992nikitanimore@gmail.com>
2024-11-26 13:07:59 +0530

    complete string lower case convert string

--- F_StringtoLowerCase.java
+++ F_StringtoLowerCase.java
@@ -1,5 +1,14 @@
 
 
 class F_StringtoLowerCase{
-   
+    static String lowerCase(String s){
+        String str = s.toLowerCase();
+        return str;
+    }
+        public static void main(String[] args) {
+            System.out.println(lowerCase("hello"));
+            String str1 = "Hello I am Nikita Nimore";
+            System.out.println(lowerCase(str1));
+        }
+        
 }
