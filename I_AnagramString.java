59cf290b7f2e84e5c07c9eef87cddb92f2a5edda
nikita nimore <992nikitanimore@gmail.com>
2024-11-28 18:30:52 +0530

    complete my anagram /string proram

--- I_AnagramString.java
+++ I_AnagramString.java
@@ -6,5 +6,45 @@ import java.util.Set;
 
 class I_AnagramString{
     
+    public static boolean areAnagrams(String s1, String s2) {
 
+        if (s1.length() != s2.length()) {
+              return false;
+          }
+          HashMap<Character, Integer> map = new HashMap<>();
+          for (int i = 0; i < s1.length(); i++) {
+      
+              if (map.containsKey(s1.charAt(i))) {
+                  
+                  map.put(s1.charAt(i),
+                          map.get(s1.charAt(i)) + 1);
+              }
+              else {
+                  
+                  map.put(s1.charAt(i), 1);
+              }
+          } 
+           for (int i = 0; i < s2.length(); i++) {
+               if (map.containsKey(s2.charAt(i))) {
+                   map.put(s2.charAt(i),
+                          map.get(s2.charAt(i)) - 1);
+              }
+          }
+          Set<Character> keys = map.keySet();
+           for (Character key : keys) {
+              if (map.get(key) != 0) {
+                  return false;
+              }
+          }
+            return true;
+      }
+      public static void main(String[] args) {
+        // I_AnagramString str = new I_AnagramString();
+        String s = "rasha";
+        String str1 = "radha";
+        System.out.println(I_AnagramString.areAnagrams(s,str1));
+        String name = "Nikita";
+        String name1 = "Nitika";
+        System.out.println(I_AnagramString.areAnagrams(name,name1));
+      }
 }
