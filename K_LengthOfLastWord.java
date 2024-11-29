5d915cba4e0cd4ea07e1d0b20743fe61f9b1d827
nikita nimore <992nikitanimore@gmail.com>
2024-11-29 19:11:23 +0530

    complete program length of last word

--- K_LengthOfLastWord.java
+++ K_LengthOfLastWord.java
@@ -1,5 +1,21 @@
 
 
 class K_LengthOfLastWord{
-
+    public int lengthOfLastWord(String s) {
+        int last = s.length()-1; 
+        while (last >= 0 && s.charAt(last) == ' ') {
+             last--;
+         }
+          int start = last;
+ 
+           while (start >= 0 && s.charAt(start) != ' ') {
+             start--;
+         }
+        return last - start;
+     }
+     public static void main(String[] args) {
+        K_LengthOfLastWord str = new K_LengthOfLastWord();
+        String str1 = "hello i am Nikita";
+        System.out.println(str.lengthOfLastWord(str1));
+     }
 }
