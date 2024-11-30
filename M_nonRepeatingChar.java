afabf5c17d77fd06b5ce640404acf100bbe27309
nikita nimore <992nikitanimore@gmail.com>
2024-11-30 22:33:00 +0530

    complete my program

--- M_nonRepeatingChar.java
+++ M_nonRepeatingChar.java
@@ -2,5 +2,25 @@
 
 
 class M_nonRepeatingChar{
-    
+    static char nonRepeatingChar(String s) {
+        // Your code here
+        int n = s.length();
+         for (int i = 0; i < n; ++i) {
+            boolean found = false;
+            for (int j = 0; j < n; ++j) {
+                if (i != j && s.charAt(i) == s.charAt(j)) {
+                    found = true;
+                    break;
+                }
+            }
+    if (found == false) 
+                return s.charAt(i);
+        }
+                return '$';
+    }
+    public static void main(String[] args) {
+        String s = "racecar";
+      
+        System.out.println(nonRepeatingChar(s));
+    }
 }
