7500af174881441d0712a347c471d5dacc7c911a
nikita nimore <992nikitanimore@gmail.com>
2024-12-03 22:39:55 +0530

    complete my program

--- O_MergeToShortList.java
+++ O_MergeToShortList.java
@@ -1,5 +1,34 @@
 
 
 class O_MergeToShortList{
-    
+    public ListNode mergeTwoLists(ListNode first, ListNode second) {
+
+     ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        // Traverse both lists
+        while (first != null && second != null) {
+            if (first.val <= second.val) {
+                current.next = first;
+                first = first.next;
+            } else {
+                current.next = second;
+                second = second.next;
+            }
+            current = current.next;
+        }
+
+        // Attach the remaining elements (if any) from list1 or list2
+        if (first != null) {
+            current.next = first;
+        } else if (second != null) {
+            current.next = second;
+        }
+
+        // Return the next node of the dummy node as the head of the merged list
+        return dummy.next;
+} 
+
+
+
 }
