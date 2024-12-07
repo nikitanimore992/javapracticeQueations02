261090f255e8329da8203c997e281ffd6f91df63
nikita nimore <992nikitanimore@gmail.com>
2024-12-07 14:48:04 +0530

    complete program

--- R_removeDuplicates.java
+++ R_removeDuplicates.java
@@ -1,5 +1,28 @@
 
 
 class removeDuplicates{
-    
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+           return 0;
+       }
+       
+       int uniqueIndex = 0; // Points to the last unique element
+       
+       for (int i = 1; i < nums.length; i++) {
+           if (nums[i] != nums[uniqueIndex]) {
+               uniqueIndex++;
+               nums[uniqueIndex] = nums[i];
+           }
+        }
+        
+        // Length of the array without duplicates
+        return uniqueIndex + 1;
+    }
+
+    public static void main(String[] args) {
+        removeDuplicates obj = new removeDuplicates();
+        int[] nums = {0, 0, 1, 1, 2, 3, 3};
+        int newLength = obj.removeDuplicates(nums);
+        System.out.println(newLength);
+    }
 }
