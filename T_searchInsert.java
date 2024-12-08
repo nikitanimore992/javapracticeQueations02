73ea71f963919fe811a8afec7ef9da694dc9ff60
nikita nimore <992nikitanimore@gmail.com>
2024-12-08 23:11:48 +0530

    complete program

--- T_searchInsert.java
+++ T_searchInsert.java
@@ -1,5 +1,29 @@
 
 
 class T_searchInsert{
-    
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return left;
+    }
+    public static void main(String[] args) {
+
+        T_searchInsert solution = new T_searchInsert();
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        int result = solution.searchInsert(nums, target);
+        System.err.println(result);
+    }
 }
