916eceb6799ea93f8fee8e1e9e02a9cd6e566e3e
nikita nimore <992nikitanimore@gmail.com>
2024-12-10 22:35:16 +0530

    complete this program

--- W_poweroftwo.java
+++ W_poweroftwo.java
@@ -1,3 +1,18 @@
 class W_poweroftwo{
-    
+    public static boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false; // Negative numbers and 0 are not powers of 2
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        int num = 16;
+        if (isPowerOfTwo(num)) {
+            System.out.println(num + " is a power of 2");
+        } else {
+            System.out.println(num + " is not a power of 2");
+        }
+    }
 }
