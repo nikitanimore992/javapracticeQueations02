b04c6d4462eb7e11f09d19e332b94347d9864fda
nikita nimore <992nikitanimore@gmail.com>
2024-12-15 23:26:40 +0530

    add first occurance second program

--- f1/N_firstOccuranceString.java
+++ f1/N_firstOccuranceString.java
@@ -20,3 +20,48 @@ class N_firstOccuranceString{
         System.out.println(str.strStr(hay, n));
       }
 }
+
+// second program gfg solution 
+/*
+ public class FirstOccurrence {
+
+    // Function to find the first occurrence of pat in txt
+    public static int firstOccurrence(String txt, String pat) {
+        if (txt == null || pat == null || txt.length() < pat.length()) {
+            return -1;
+        }
+
+        int n = txt.length();
+        int m = pat.length();
+
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+            for (j = 0; j < m; j++) {
+                if (txt.charAt(i + j) != pat.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == m) { // If the entire pattern is matched
+                return i;
+            }
+        }
+
+        return -1; // No match found
+    }
+
+    // Main method to test the function
+    public static void main(String[] args) {
+        String txt = "geeksforgeeks";
+        String pat = "for";
+
+        int result = firstOccurrence(txt, pat);
+
+        if (result != -1) {
+            System.out.println("First occurrence of pattern is at index: " + result);
+        } else {
+            System.out.println("Pattern not found in the text.");
+        }
+    }
+}
+
+ */
