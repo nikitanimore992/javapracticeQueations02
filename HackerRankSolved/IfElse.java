eea7d55d5e5ea87824d0b3a579b9260051c59ab1
nikita nimore <992nikitanimore@gmail.com>
2025-08-13 12:21:18 +0530

    ifelse

---
+++ HackerRankSolved/IfElse.java
@@ -0,0 +1,25 @@
+// Task
+// Given an integer, , perform the following conditional actions:
+
+// If  is odd, print Weird
+// If  is even and in the inclusive range of 2 to 5, print Not Weird
+// If  is even and in the inclusive range of 6 to 20, print Weird
+// If  is even and greater than 20 , print Not Weird
+// Complete the stub code provided in your editor to print whether or not  is weird.
+
+
+
+import java.util.Scanner;
+
+class IfElse{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+        if((a%2!=0)||(a>=6 && a<=20)){
+            System.out.println("Weird");
+        }else{
+            System.out.println("Not Weird");
+        }
+    }
+}
