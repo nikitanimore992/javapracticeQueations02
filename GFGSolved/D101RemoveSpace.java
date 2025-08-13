2414353d5595458a756a460230aa11e5de224a6d
nikita nimore <992nikitanimore@gmail.com>
2025-08-13 15:00:58 +0530

    remove space

---
+++ GFGSolved/D101RemoveSpace.java
@@ -0,0 +1,13 @@
+
+
+class D101RemoveSpace{
+    // String Modify(String s){
+    //     s=s.replaceAll(" ","");
+    // }
+    public static void main(String[] args){
+        // Modify removespace = new Modify();
+        String str = "Geeks for geeks";
+        str=str.replaceAll(" ","");
+        System.out.println(str);
+    }
+}
