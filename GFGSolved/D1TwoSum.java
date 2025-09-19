bad7d83aaa9ddeb755807f80d516c2873a7dedd3
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-19 23:56:59 +0530

    complete the solution

--- GFGSolved/D1TwoSum.java
+++ GFGSolved/D1TwoSum.java
@@ -1,4 +1,9 @@
-class D1TwoSum{
+import java.util.*;  
+import java.util.*;  
+import java.util.Map;
+import java.util.HashMap;
+
+class D1TwoSum {
   public int[] twoSum(int[] nums, int target) {
     Map<Integer, Integer> numToIndex = new HashMap<>();
 
@@ -8,6 +13,18 @@ class D1TwoSum{
       numToIndex.put(nums[i], i);
     }
 
-    throw new IllegalArgumentException();
+    throw new IllegalArgumentException("No two sum solution");
+  }
+
+  // Main method for testing
+  public static void main(String[] args) {
+    D1TwoSum solver = new D1TwoSum();
+
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+
+    int[] result = solver.twoSum(nums, target);
+
+    System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
   }
 }
