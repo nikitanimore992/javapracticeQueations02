53c4ead2676aa1122d48e0160ff42a9d573c45ea
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-21 19:38:40 +0530

    commit someting

--- GFGSolved/D1TwoSum.java
+++ GFGSolved/D1TwoSum.java
@@ -16,15 +16,13 @@ class D1TwoSum {
     throw new IllegalArgumentException("No two sum solution");
   }
 
+  
   // Main method for testing
   public static void main(String[] args) {
     D1TwoSum solver = new D1TwoSum();
-
     int[] nums = {2, 7, 11, 15};
     int target = 9;
-
     int[] result = solver.twoSum(nums, target);
-
     System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
   }
 }
