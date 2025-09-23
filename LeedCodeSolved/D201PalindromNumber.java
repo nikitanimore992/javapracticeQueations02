1fd9f3dd245ebc2530096bdc3c5486c049c85a5e
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-24 00:05:35 +0530

    new filepalindrom number

---
+++ LeedCodeSolved/D201PalindromNumber.java
@@ -0,0 +1,26 @@
+class D201PalindromNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false; // negative numbers not palindrome
+
+        int original = x;
+        int reversed = 0;
+
+        while (x > 0) {
+            int digit = x % 10;           // last digit
+            reversed = reversed * 10 + digit; 
+            x = x / 10;                   // remove last digit
+        }
+
+        return original == reversed;  // compare original and reversed
+    }
+
+    public static void main(String[] args) {
+        D201PalindromNumber solver = new D201PalindromNumber();
+
+        int[] testNumbers = {121, 123, 1001, 10, -121};
+
+        for (int num : testNumbers) {
+            System.out.println("Is " + num + " a palindrome? " + solver.isPalindrome(num));
+        }
+    }
+}
