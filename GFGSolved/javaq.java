d001f1001f0b1d5e778a8dc84aec987be7fcff41
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-24 23:59:34 +0530

    create new file

---
+++ GFGSolved/javaq.java
@@ -0,0 +1,18 @@
+class javaq{
+    public static void main(String[] args) {
+    //byte b=128;//range of byte is -128 to 127
+    byte  b=34;
+    short a=b;
+    int   s=a;
+    long  l=s;
+    float f=l;  
+    double d=f;
+        System.out.println("byte b = "+b);
+            System.out.println("short a= b "+a);
+                System.out.println("int s = b "+s);
+                    System.out.println("long l = b "+l);
+                        System.out.println("float f = b "+f);
+                            System.out.println("double d = b "+d);
+
+    }
+}
