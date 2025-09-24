331c7da0e8e6395c845b0fd80ec11689338cc316
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-25 00:16:07 +0530

    clibing stairs

---
+++ LeedCodeSolved/D301ClibingStair.java
@@ -0,0 +1,23 @@
+class D301ClibingStair {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int first = 1, second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+    public static void main(String[] args) {
+        D301ClibingStair Nikita = new D301ClibingStair();
+        
+        System.out.println("Climbing 2 stairs: " + Nikita.climbStairs(2)); // 2
+        System.out.println("Climbing 3 stairs: " + Nikita.climbStairs(3)); // 3
+        System.out.println("Climbing 4 stairs: " + Nikita.climbStairs(4)); // 5
+        System.out.println("Climbing 5 stairs: " + Nikita.climbStairs(5)); // 8
+        System.out.println("Climbing 10 stairs: " + Nikita.climbStairs(10)); // 89
+    }
+}
