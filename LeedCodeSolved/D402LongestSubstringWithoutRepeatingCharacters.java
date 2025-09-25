4e9703e0c0197b0d3295c77cd0b04e3c01725177
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-25 18:47:02 +0530

    D402LongestSubstringWithoutRepeatingCharacters

--- LeedCodeSolved/D402LongestSubstringWithoutRepeatingCharacters.java
+++ LeedCodeSolved/D402LongestSubstringWithoutRepeatingCharacters.java
@@ -1,3 +1,32 @@
+
+import java.util.*;
 class D402LongestSubstringWithoutRepeatingCharacters{
-    
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            // If duplicate, shrink window from left
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(c);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        D402LongestSubstringWithoutRepeatingCharacters solver = new D402LongestSubstringWithoutRepeatingCharacters();
+
+        System.out.println(solver.lengthOfLongestSubstring("abcabcbb")); // 3
+        System.out.println(solver.lengthOfLongestSubstring("bbbbb"));   // 1
+        System.out.println(solver.lengthOfLongestSubstring("pwwkew"));  // 3
+    }
+
 }
