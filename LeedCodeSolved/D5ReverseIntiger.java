c3f6d2647b45b1f368a7a53684b5215deb080153
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-26 23:51:32 +0530

    reverse integer

--- LeedCodeSolved/D5ReverseIntiger.java
+++ LeedCodeSolved/D5ReverseIntiger.java
@@ -1,3 +1,30 @@
 class D5ReverseIntiger{
-    
+    public int reverse(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;        
+            x = x / 10;                
+
+            if (reversed > Integer.MAX_VALUE / 10 || 
+                (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0; // overflow
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || 
+                (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0; // underflow
+            }
+
+            reversed = reversed * 10 + digit;
+        }
+
+        return reversed;
+    } 
+    public static void main(String args[]){
+        D5ReverseIntiger solution = new D5ReverseIntiger();
+        int num1 = solution.reverse(123);
+        int num2 = solution.reverse(123456);
+        System.out.println(num1);
+        System.out.println(num2);
+    } 
 }
